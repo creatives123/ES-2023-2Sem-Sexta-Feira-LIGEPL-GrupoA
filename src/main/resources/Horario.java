af05008eedcaa09c5577e3bfe7aa1af9de14f291
alexmleitao <alexmleitao@gmail.com>
2023-04-08 16:22:22 +0100

    Criar Objeto Horario

---
+++ src/main/resources/Horario.java
@@ -0,0 +1,29 @@
+public class Horario {
+    public String curso;
+    public String unidadeCurricular;
+    public String turno;
+    public String turma;
+    public int inscritos;
+    public String diaSemana;
+    public String horaInicio;
+    public String horaFim;
+    public String dataAula;
+    public String sala;
+    public int lotacao;
+
+    public Horario(String curso, String unidadeCurricular, String turno, String turma,
+                   int inscritos, String diaSemana, String horaInicio, String horaFim,
+                   String dataAula, String sala, int lotacao) {
+        this.curso = curso;
+        this.unidadeCurricular = unidadeCurricular;
+        this.turno = turno;
+        this.turma = turma;
+        this.inscritos = inscritos;
+        this.diaSemana = diaSemana;
+        this.horaInicio = horaInicio;
+        this.horaFim = horaFim;
+        this.dataAula = dataAula;
+        this.sala = sala;
+        this.lotacao = lotacao;
+    }
+}
