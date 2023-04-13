3d280c76c5a712ce1d955296c2e95ef82e499313
alexmleitao <alexmleitao@gmail.com>
2023-04-13 18:25:00 +0100

    Comentários JavaDoc e Testes
    Comentários à Classe Horario, construção de JavaDoc e testes à classe

--- src/main/java/services/HorarioJsonWriter.java
+++ src/main/java/services/HorarioJsonWriter.java
@@ -7,21 +7,35 @@ import java.util.List;
 
 import models.Horario;
 
+/**
+ * Classe utilitária responsável por escrever objetos do tipo {@link Horario} em
+ * formato JSON.
+ * Utiliza a biblioteca Jackson para converter os objetos em bytes
+ * correspondentes ao JSON gerado.
+ */
 public class HorarioJsonWriter {
 
-    //private static String filePath = "src/main/resources/teste.json";
+    // Construtor privado para impedir instanciação da classe
+    private HorarioJsonWriter() {
+    }
 
+    /**
+     * Converte uma lista de objetos do tipo {@link Horario} em bytes
+     * correspondentes ao formato JSON gerado.
+     * <p>
+     * @param horarios Lista de objetos do tipo {@link Horario} a serem convertidos
+     *                 em JSON.
+     * @return Bytes correspondentes ao JSON gerado a partir dos objetos fornecidos.
+     */
     public static byte[] writeToJson(List<Horario> horarios) {
+        // Cria um objeto ObjectMapper para converter os objetos em JSON
         ObjectMapper mapper = new ObjectMapper();
 
         try {
-            //mapper.writeValue(new File(filePath), horarios);
-
-            // Nós queremos que gere um ficheiro para fazer download na pagina
-            // para isso precisamos que retorne os bytes e depois do
-            // lado da aplicação converte em ficehiro para download
+            // Converte a lista de objetos em bytes correspondentes ao JSON gerado
             return mapper.writeValueAsBytes(horarios);
         } catch (IOException e) {
+            // Em caso de exceção, imprime o stack trace e retorna um array vazio de bytes
             e.printStackTrace();
         }
         return new byte[0];
--- src/test/java/HorarioJsonWriterTest.java
+++ src/test/java/HorarioJsonWriterTest.java
@@ -1,10 +1,14 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import models.Horario;
 import services.HorarioJsonWriter;
 
-import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,20 +19,30 @@ class HorarioJsonWriterTest {
     @Test
     void writeToJson_WithValidHorarios_ShouldCreateJsonFile() throws IOException {
         // Arrange
-         List<Horario> horarios = new ArrayList<>();
-        // horarios.add(new Horario("1", "segunda", "9:00", "11:00", 2, "sala 1", "professor 1", "disciplina 1", "curso 1", "periodo 1", 20));
-        // horarios.add(new Horario("2", "terça", "9:00", "11:00", 2, "sala 2", "professor 2", "disciplina 2", "curso 2", "periodo 2", 30));
+        List<Horario> horarios = new ArrayList<Horario>();
+        horarios.add(new Horario(new String[] { "curso 1", "unidadeCurricular 1", "turno 1", "turma 1", "20", "segunda",
+                "9:00", "11:00", "dataAula 1", "sala 1", "30" }));
+        horarios.add(new Horario(new String[] { "curso 2", "unidadeCurricular 2", "turno 2", "turma 2", "30", "terça",
+                "11:00", "13:00", "dataAula 2", "sala 2", "40" }));
+        String expectedJson = "[{\"curso\":[\"curso 1\"],\"unidadeCurricular\":\"unidadeCurricular 1\",\"turno\":\"turno 1\",\"turma\":[\"turma 1\"],\"inscritos\":20,\"diaSemana\":\"segunda\",\"horaInicio\":\"9:00\",\"horaFim\":\"11:00\",\"dataAula\":\"dataAula 1\",\"sala\":\"sala 1\",\"lotacao\":30},{\"curso\":[\"curso 2\"],\"unidadeCurricular\":\"unidadeCurricular 2\",\"turno\":\"turno 2\",\"turma\":[\"turma 2\"],\"inscritos\":30,\"diaSemana\":\"terça\",\"horaInicio\":\"11:00\",\"horaFim\":\"13:00\",\"dataAula\":\"dataAula 2\",\"sala\":\"sala 2\",\"lotacao\":40}]";
 
         // Act
         HorarioJsonWriter.writeToJson(horarios);
 
         // Assert
-        File file = new File(testFilePath);
-        //assertTrue(file.exists());
-        //assertTrue(file.isFile());
+        byte[] actualBytes = HorarioJsonWriter.writeToJson(horarios);
+        String actualJson = new String(actualBytes);
+        assertEquals(expectedJson, actualJson);
 
-        // Clean up
-        file.delete();
+        // Write bytes to file
+        FileOutputStream outputStream = new FileOutputStream(testFilePath);
+        outputStream.write(actualBytes);
+        outputStream.close();
+
+        // Read file contents
+        String fileContents = new String(Files.readAllBytes(Paths.get(testFilePath)));
+        assertEquals(expectedJson, fileContents);
+
     }
 
     @Test
@@ -37,6 +51,7 @@ class HorarioJsonWriterTest {
         List<Horario> horarios = null;
 
         // Assert
-        //assertThrows(NullPointerException.class, () -> teste.HorarioJsonWriter.writeToJson(horarios));
+        assertThrows(NullPointerException.class, () -> HorarioJsonWriter.writeToJson(horarios));
     }
+
 }
