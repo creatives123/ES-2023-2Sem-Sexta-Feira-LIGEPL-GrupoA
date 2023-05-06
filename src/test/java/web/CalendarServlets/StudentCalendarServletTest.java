3b7cd037d67a1bcc885f1e9b7f9d80a17c5abc0c
tonistone <102620389+tonistone@users.noreply.github.com>
2023-05-06 23:44:31 +0100

    Foram efetuados mais testes.

--- src/test/java/web/CalendarServlets/StudentCalendarServletTest.java
+++ src/test/java/web/CalendarServlets/StudentCalendarServletTest.java
@@ -1,6 +1,7 @@
 package web.CalendarServlets;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -73,15 +74,28 @@ public class StudentCalendarServletTest {
         horario.setUnidadeCurricular("Programacao");
         horario.setTurno("T1");
         horario.setTurma(Arrays.asList("1"));
-        horario.setInscritos(50);
+        horario.setInscritos(60);   
         horario.setDiaSemana("Segunda-feira");
         horario.setHoraInicio("09:00:00");
         horario.setHoraFim("11:00:00");
         horario.setDataAula("01/01/2022");
         horario.setSala("B102");
-        horario.setLotacao(60);
+        horario.setLotacao(50);
 
-        horarios.add(horario);
+        Horario horario2 = new Horario();
+        horario2.setCurso(Arrays.asList("LEI"));
+        horario2.setUnidadeCurricular("Programacao");
+        horario2.setTurno("T1");
+        horario2.setTurma(Arrays.asList("1"));
+        horario2.setInscritos(60);
+        horario2.setDiaSemana("Segunda-feira");
+        horario2.setHoraInicio("10:00:00");
+        horario2.setHoraFim("11:00:00");
+        horario2.setDataAula("01/01/2022");
+        horario2.setSala("B102");
+        horario2.setLotacao(50);
+
+        horarios.add(horario2);
 
         List<CalendarModel> calendario = new ArrayList<>();
         CalendarModel cal = new CalendarModel();
@@ -91,6 +105,14 @@ public class StudentCalendarServletTest {
         cal.setStart("2022-01-01 09:00:00");
         cal.setEnd("2022-01-01 11:00:00");
         calendario.add(cal);
+
+        CalendarModel cal2 = new CalendarModel();
+        cal2.setHorario(horario2);
+        cal2.setColor("blue");
+        cal2.setTitle("Horario  df");
+        cal2.setStart("2022-01-01 10:00:00");
+        cal2.setEnd("2022-01-01 11:00:00");
+        calendario.add(cal2);
 
         HttpSession session = mock(HttpSession.class);
         when(request.getSession()).thenReturn(session);
@@ -111,4 +133,95 @@ public class StudentCalendarServletTest {
         CalendarWrapper wrapper = new ObjectMapper().readValue(stringWriter.toString().trim(), CalendarWrapper.class);
         assertEquals(wrapper.getEvents().size(), horarios.size());
     }
+
+
+    @Test
+    public void testAddEventWithout_adding() throws IOException {
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        
+        // Cria uma lista de horarios
+        List<Horario> horarios = new ArrayList<>();
+        List<Horario> horarios2 = new ArrayList<>();        
+
+        Horario horario = new Horario();
+        horario.setCurso(Arrays.asList("LEI"));
+        horario.setUnidadeCurricular("Programacao");
+        horario.setTurno("T1");
+        horario.setTurma(Arrays.asList("1"));
+        horario.setInscritos(60);   
+        horario.setDiaSemana("Segunda-feira");
+        horario.setHoraInicio("09:00:00");
+        horario.setHoraFim("11:00:00");
+        horario.setDataAula("01/01/2022");
+        horario.setSala("B102");
+        horario.setLotacao(50);
+        horarios.add(horario);
+
+        Horario horario2 = new Horario();
+        horario2.setCurso(Arrays.asList("LEIa"));
+        horario2.setUnidadeCurricular("Programacao");
+        horario2.setTurno("T1");
+        horario2.setTurma(Arrays.asList("1"));
+        horario2.setInscritos(60);
+        horario2.setDiaSemana("Segunda-feira");
+        horario2.setHoraInicio("10:00:00");
+        horario2.setHoraFim("11:00:00");
+        horario2.setDataAula("01/01/2022");
+        horario2.setSala("B102");
+        horario2.setLotacao(50);
+        horarios2.add(horario2);
+
+        HttpSession session = mock(HttpSession.class);
+        when(request.getSession()).thenReturn(session);
+
+        session.setAttribute("horarios", horarios);
+        when(session.getAttribute("horarios")).thenReturn(horarios);
+        
+        session.setAttribute("student_horario", horarios);
+        when(session.getAttribute("student_horario")).thenReturn(horarios2);
+
+        when(request.getParameter("course")).thenReturn("LEI");
+        when(request.getParameter("uc")).thenReturn("Programacao");
+        when(request.getParameter("turno")).thenReturn("T1");
+    
+        new StudentCalendarServlet().doPost(request, response);
+    }
+
+    @Test
+    public void testDeleteEvents() throws IOException {
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        
+        // Cria uma lista de horarios
+        List<Horario> horarios = new ArrayList<>();
+
+        Horario horario = new Horario();
+        horario.setCurso(Arrays.asList("LEI"));
+        horario.setUnidadeCurricular("Programacao");
+        horario.setTurno("T1");
+        horario.setTurma(Arrays.asList("1"));
+        horario.setInscritos(60);   
+        horario.setDiaSemana("Segunda-feira");
+        horario.setHoraInicio("09:00:00");
+        horario.setHoraFim("11:00:00");
+        horario.setDataAula("01/01/2022");
+        horario.setSala("B102");
+        horario.setLotacao(50);
+        horarios.add(horario);
+
+        HttpSession session = mock(HttpSession.class);
+        when(request.getSession()).thenReturn(session);
+
+        session.setAttribute("student_horario", horarios);
+        when(session.getAttribute("student_horario")).thenReturn(horarios);
+
+        when(request.getParameter("uc")).thenReturn("Programacao");
+        when(request.getParameter("turno")).thenReturn("T1");
+    
+        new StudentCalendarServlet().doDelete(request, response);
+    }
+    
 }
