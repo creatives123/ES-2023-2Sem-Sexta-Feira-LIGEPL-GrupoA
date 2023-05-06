b8609b01ce232ef83a0dad4610a724565bbfeea6
mariapc01 <100026819+mariapc01@users.noreply.github.com>
2023-05-06 20:26:09 +0100

    update a botões de download

--- src/main/java/web/CalendarServlets/WebCallImporterServlet.java
+++ src/main/java/web/CalendarServlets/WebCallImporterServlet.java
@@ -32,19 +32,26 @@ import net.fortuna.ical4j.model.component.VEvent;
 
 public class WebCallImporterServlet extends HttpServlet {
   public static final String WEBCAL_HORARIO ="WebcalHorario";
-
+/**
+    Este método é usado para tratar 'GET requests' do cliente.
+    Remove o atributo "WEBCAL_HORARIO" da sessão e importa eventos de calendários de um URL introduzido pelo utilizador,
+    usando o método "importFromUrl(String, HttpServletRequest)".
+    A mensagem proveniente desse método resulta message is set as a session attribute "messageUpload" and the user is redirected to the index.jsp page.
+    @param request {@link HttpServletRequest} é o objeto que contém o 'request' do cliente
+    @param response {@link HttpServletResponse} é o objeto que o contém a 'response' do 'servlet's response
+    @throws IOException if an input or output exception occurs while the servlet is handling the GET request
+    */
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)  throws IOException {
+    // Caso o atributo "WEBCAL_HORARIO" esteja preenchido, é descartado
     request.getSession().removeAttribute(WEBCAL_HORARIO);
     String message ="";
-          String uri = request.getParameter("uri");
-            
-            message = importFromUrl(uri, request);
- 
-            request.getSession().setAttribute("messageUpload", message);
-            response.sendRedirect(request.getContextPath() + "/index.jsp");
-            
-        
+    String uri = request.getParameter("uri");
+    // Import calendar events from the user-provided URL
+     message = importFromUrl(uri, request);
+     // Set the resulting message as a session attribute "messageUpload" and redirect the user to the index.jsp page
+     request.getSession().setAttribute("messageUpload", message);
+     response.sendRedirect(request.getContextPath() + "/index.jsp");   
   }
 
   public static String importFromUrl(String uri, HttpServletRequest request) {
