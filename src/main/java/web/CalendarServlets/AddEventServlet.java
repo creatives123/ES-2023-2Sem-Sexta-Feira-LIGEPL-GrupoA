c9f2039a479810926b26748778e96443c9dbb847
alexmleitao <alexmleitao@gmail.com>
2023-05-06 12:51:09 +0100

    Update AddEventServlet.java
    JavaDocs para adicionar eventos

--- src/main/java/web/CalendarServlets/AddEventServlet.java
+++ src/main/java/web/CalendarServlets/AddEventServlet.java
@@ -10,8 +10,22 @@ import javax.servlet.http.HttpServletResponse;
 import models.Horario;
 import services.CommonManager;
 
+/**
+ * A classe AddEventServlet é um servlet que trata os pedidos GET para adicionar um evento ao calendário de um estudante.
+ * 
+ * Este servlet recebe os parâmetros do pedido (curso, unidade curricular e turno) e verifica se existe um evento correspondente na lista
+ * de eventos do calendário global da aplicação. Se houver um evento correspondente e o estudante ainda não tiver adicionado este evento ao seu calendário,
+ * o evento é adicionado ao calendário do estudante na sessão.
+ */
 public class AddEventServlet extends HttpServlet {
 
+    /**
+     * Trata os pedidos GET para adicionar um evento ao calendário de um estudante.
+     * 
+     * @param request o objeto HttpServletRequest contendo informações sobre o pedido
+     * @param response o objeto HttpServletResponse utilizado para enviar a resposta
+     * @throws IOException se ocorrer um erro ao ler ou escrever no fluxo de entrada ou saída
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
       
@@ -19,12 +33,12 @@ public class AddEventServlet extends HttpServlet {
         String uc = request.getParameter("uc");
         String turno = request.getParameter("turno");
 
-        List<Horario> events = CommonManager.getHorariosFromSession(request.getSession());
-        List<Horario> studentSchedule = CommonManager.getStudentHorarioFromSession(request.getSession());
+        List<Horario> eventos = CommonManager.getHorariosFromSession(request.getSession());
+        List<Horario> horarioDoEstudante = CommonManager.getStudentHorarioFromSession(request.getSession());
 
-        for(Horario h : events) {
+        for(Horario h : eventos) {
             if (h.getCurso().contains(curso) && h.getUnidadeCurricular().equals(uc) && h.getTurno().equals(turno)
-                    && !studentSchedule.contains(h)) {
+                    && !horarioDoEstudante.contains(h)) {
                 CommonManager.addToStudentHorarioFromSession(request.getSession(), h);
             }
         }
