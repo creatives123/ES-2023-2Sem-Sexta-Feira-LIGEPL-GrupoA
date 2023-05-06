7f7bd158a75f1f7d467d2c88ca7db7045d2f71e4
mariapc01 <100026819+mariapc01@users.noreply.github.com>
2023-05-06 23:49:36 +0100

    Correção de cores no StudentCalendarServlet

--- src/main/java/web/CalendarServlets/StudentCalendarServlet.java
+++ src/main/java/web/CalendarServlets/StudentCalendarServlet.java
@@ -123,13 +123,13 @@ public class StudentCalendarServlet extends HttpServlet {
                 String t = h.getUnidadeCurricular().concat(" ").concat(h.getTurno());
                 if (h.equals(m.getHorario())) {
                     if (m.getHorario().isOverCrowded()) {
-                        m.setColor("#FF0000");
+                        m.setColor("#B40A7A");
                         addVal(eventosLotados, t);
                     }
                 }
                 else {
                     if (DateManager.sameInterval(m.getHorario(),h)) {
-                        m.setColor("orange");
+                        m.setColor("#FFD37F");
                         addVal(eventosSobrepostos, t);
                     }
                 }
