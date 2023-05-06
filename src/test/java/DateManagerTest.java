ec0fc2071f74902e046036989adede8b1e90b349
alexmleitao <alexmleitao@gmail.com>
2023-05-06 19:45:01 +0100

    Update DateManagerTest.java
    Correções no DataManager

--- src/test/java/DateManagerTest.java
+++ src/test/java/DateManagerTest.java
@@ -1,16 +1,149 @@
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import models.Horario;
 import services.DateManager;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-public class DateManagerTest {
+class DateManagerTest {
 
     @Test
-    public void testGetScheduleCorrectTimeFormat() {
-        String formattedDate = DateManager.getScheduleCorrectTimeFormat("10/05/2022 14:30:00");
-        assertNotNull(formattedDate);
-        assertEquals("2022-05-10 14:30:00", formattedDate);
+    void testGetScheduleCorrectTimeFormat() {
+        // Arrange
+        String input = "10/05/2023 14:30:00";
+        String expectedOutput = "2023-05-10 14:30:00";
+        
+        // Act
+        String actualOutput = DateManager.getScheduleCorrectTimeFormat(input);
+        
+        // Assert
+        assertEquals(expectedOutput, actualOutput);
     }
-}
+
+    @Test
+    void testGetDataHoraInicio() {
+        // Arrange
+        Horario h = new Horario();
+        h.setDataAula("10/05/2023");
+        h.setHoraInicio("14:30:00");
+        String expectedOutput = "2023-05-10 14:30:00";
+        
+        // Act
+        String actualOutput = DateManager.getDataHoraInicio(h);
+        
+        // Assert
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void testGetDataHoraInicioNull() {
+        // Arrange
+        Horario h = null;
+        Executable executable = () -> DateManager.getDataHoraInicio(h);
+
+        // Act & Assert
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, executable);
+        assertEquals("h is null", exception.getMessage());
+    }
+
+    @Test
+    void testGetDataHoraFim() {
+        // Arrange
+        Horario h = new Horario();
+        h.setDataAula("10/05/2023");
+        h.setHoraFim("16:30:00");
+        String expectedOutput = "2023-05-10 16:30:00";
+        
+        // Act
+        String actualOutput = DateManager.getDataHoraFim(h);
+        
+        // Assert
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void testGetDataHoraFimNull() {
+        // Arrange
+        Horario h = null;
+        Executable executable = () -> DateManager.getDataHoraFim(h);
+
+        // Act & Assert
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, executable);
+        assertEquals("h is null", exception.getMessage());
+    }
+
+    @DisplayName("Test sameInterval method")
+    @ParameterizedTest(name = "test {index}: h1={0}, h2={1}, expected={2}")
+    @MethodSource("sameIntervalArguments")
+    void testSameInterval(Horario h1, Horario h2, boolean expected) {
+        // Act
+        boolean actual = DateManager.sameInterval(h1, h2);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    static Collection<Object[]> sameIntervalArguments() {
+        Horario h1 = new Horario();
+        h1.setDataAula("10/05/2023");
+        h1.setHoraInicio("14:30:00");
+        h1.setHoraFim("16:30:00");
+
+        Horario h2 = new Horario();
+        h2.setDataAula("10/05/2023");
+        h2.setHoraInicio("16:00:00");
+        h2.setHoraFim("18:00:00");
+
+        Horario h3 = new Horario();
+        h3.setDataAula("11/05/2023");
+        h3.setHoraInicio("09:00:00");
+        h3.setHoraFim("12:00:00");
+    
+        Horario h4 = new Horario();
+        h4.setDataAula("10/05/2023");
+        h4.setHoraInicio("12:00:00");
+        h4.setHoraFim("14:00:00");
+    
+        Horario h5 = new Horario();
+        h5.setDataAula("11/05/2023");
+        h5.setHoraInicio("08:00:00");
+        h5.setHoraFim("09:00:00");
+    
+        Horario h6 = new Horario();
+        h6.setDataAula("11/05/2023");
+        h6.setHoraInicio("09:00:00");
+        h6.setHoraFim("12:00:00");
+    
+        Horario h7 = new Horario();
+        h7.setDataAula("11/05/2023");
+        h7.setHoraInicio("12:00:00");
+        h7.setHoraFim("15:00:00");
+    
+        return Arrays.asList(new Object[][] {
+            {h1, h2, true},
+            {h2, h1, true},
+            {h1, h3, false},
+            {h3, h1, false},
+            {h1, h4, true},
+            {h4, h1, true},
+            {h3, h4, true},
+            {h4, h3, true},
+            {h5, h6, true},
+            {h6, h5, true},
+            {h6, h7, false},
+            {h7, h6, false},
+            {null, h1, false},
+            {h1, null, false},
+            {null, null, false},
+        });
+    }
+}    
