24a48e768a6f8b23099940f9548ba0dba3e5401b
alexmleitao <alexmleitao@gmail.com>
2023-05-06 16:05:53 +0100

    Update DateManager.java
    JavaDocs

--- src/main/java/services/DateManager.java
+++ src/main/java/services/DateManager.java
@@ -7,9 +7,22 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
+/**
+ * Classe utilitária que fornece métodos para gerir datas.
+ */
 public class DateManager {
+
+    /**
+     * Construtor privado para evitar a instanciação da classe.
+     */
     private DateManager() {}
 
+    /**
+     * Converte uma string no formato "dd/MM/yyyy" para um objeto {@link Date}.
+     *
+     * @param input a string a ser convertida.
+     * @return o objeto {@link Date} correspondente.
+     */
     public static Date castStringToDate(String input) {
         DateFormat inputFormat = new SimpleDateFormat("dd/MM/yyyy");
         Date date = null;
@@ -27,6 +40,12 @@ public class DateManager {
     }
     
 
+    /**
+     * Converte uma string no formato "E MMM dd yyyy HH:mm:ss" para um objeto {@link Date}.
+     *
+     * @param input a string a ser convertida.
+     * @return o objeto {@link Date} correspondente.
+     */
     public static Date castStringExtendToDate(String input) {
         DateFormat inputFormat = new SimpleDateFormat("E MMM dd yyyy HH:mm:ss");
         Date date = null;
@@ -43,6 +62,12 @@ public class DateManager {
         return date;
     }
 
+    /**
+     * Converte uma string no formato "dd/MM/yyyy HH:mm:ss" para uma string no formato "yyyy-MM-dd HH:mm:ss".
+     *
+     * @param input a string a ser convertida.
+     * @return a string no formato "yyyy-MM-dd HH:mm:ss".
+     */
     public static String getScheduleCorrectTimeFormat(String input) {
         DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         LocalDateTime dateTime = LocalDateTime.parse(input, inputFormat);
