7699afa21bde567c8ab96d39bc2c7b8f88a10f7a
alexmleitao <alexmleitao@gmail.com>
2023-05-06 14:09:43 +0100

    Update HorarioToCalendarTranslator.java
    JavaDocs

--- src/main/java/Translators/HorarioToCalendarTranslator.java
+++ src/main/java/Translators/HorarioToCalendarTranslator.java
@@ -10,6 +10,12 @@ public class HorarioToCalendarTranslator {
     
     private HorarioToCalendarTranslator() {}
 
+    /**
+     * Converte um objeto do tipo Horario em um objeto do tipo CalendarModel.
+     * 
+     * @param h o objeto do tipo Horario a ser convertido
+     * @return o objeto do tipo CalendarModel correspondente ao objeto Horario
+     */
     public static CalendarModel translateHorarioToCalendar(Horario h) {
         CalendarModel c = new CalendarModel();
         c.setHorario(h);
@@ -20,6 +26,12 @@ public class HorarioToCalendarTranslator {
         return c;
     }
 
+    /**
+     * Converte uma lista de objetos do tipo Horario em uma lista de objetos do tipo CalendarModel.
+     * 
+     * @param hs a lista de objetos do tipo Horario a serem convertidos
+     * @return a lista de objetos do tipo CalendarModel correspondentes aos objetos Horario
+     */
     public static List<CalendarModel> translateHorariosToCalendars(List<Horario> hs) {
         
         List<CalendarModel> calendars = new ArrayList<>();
