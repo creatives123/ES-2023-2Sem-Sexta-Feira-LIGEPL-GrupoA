c5a2ca7177f8884294baf49ab72aefac2f77345a
alexmleitao <alexmleitao@gmail.com>
2023-05-06 21:57:07 +0100

    Test for GetCalendar

---
+++ src/test/java/web/CalendarServlets/GetCalendarServletTest.java
@@ -0,0 +1,53 @@
+package web.CalendarServlets;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GetCalendarServletTest {
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private HttpSession session;
+
+    @Test
+    public void testDoGet() throws IOException {
+        GetCalendarServlet servlet = new GetCalendarServlet();
+
+        when(request.getParameter("horario")).thenReturn("horarios");
+        when(request.getSession()).thenReturn(session);
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+
+        when(response.getWriter()).thenReturn(writer);
+
+        servlet.doGet(request, response);
+
+        verify(response, times(1)).setContentType("application/json");
+        verify(response, times(1)).setCharacterEncoding("UTF-8");
+
+        writer.flush();
+        String jsonResponse = stringWriter.toString();
+
+        // You may need to adjust this assertion to match the expected JSON output based on your test data
+        assertEquals("[]", jsonResponse);
+    }
+}
