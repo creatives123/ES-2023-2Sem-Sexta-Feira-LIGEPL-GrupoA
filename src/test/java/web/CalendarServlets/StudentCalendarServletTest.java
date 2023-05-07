d64204450bc1b7f378d87034bd64eac2aa7b66e9
mariapc01 <100026819+mariapc01@users.noreply.github.com>
2023-05-07 01:31:09 +0100

    Javadoc StudentCalendarServletTest

--- src/test/java/web/CalendarServlets/StudentCalendarServletTest.java
+++ src/test/java/web/CalendarServlets/StudentCalendarServletTest.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -32,11 +31,22 @@ public class StudentCalendarServletTest {
     @Mock
     HttpServletRequest request;
 
+    /**
+     * Este método inicializa os objetos Mock usados nos testes.
+     */
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
     }
 
+    /**
+     * Testa o método doGet() da classe StudentCalendarServlet.
+     * Testa o cenário em que não há eventos na sessão.
+     * Verifica se o servlet retorna uma string JSON esperada que representa 
+     * uma lista vazia de eventos e mapas vazios para eventos sobrepostos
+     * e superlotados.
+     * @throws IOException se ocorrer um erro de I/O durante o teste
+     */
     @Test
     public void testDoGetWithEqualEvent_Empty() throws IOException {
 
@@ -58,6 +68,15 @@ public class StudentCalendarServletTest {
         assertEquals(expectedJson, stringWriter.toString().trim());
     }
 
+    /**
+    * Testa o comportamento do método doGet() na classe StudentCalendarServlet quando
+    * há eventos iguais na sessão. Cria uma lista de horários de aula, adiciona dois
+    * horários com a mesma hora e verfica se o servlet retorna uma lista de eventos
+    * com o mesmo número de elementos que a lista de horários, indicando que não houve
+    * remoção de horários duplicados.
+    *
+    * @throws IOException se ocorrer um erro de I/O durante o teste
+    */
     @Test
     public void testDoGetWithEqualEvent() throws IOException {
 
@@ -131,7 +150,11 @@ public class StudentCalendarServletTest {
         assertEquals(wrapper.getEvents().size(), horarios.size());
     }
 
-
+    /**
+     * Testa o método doPost() da classe StudentCalendarServlet ao adicionar um evento sem adicioná-lo à lista de horários do estudante.
+     * Verifica se o evento não é adicionado à lista e se o método retorna verdadeiro.
+     * @throws IOException se ocorrer uma exceção de entrada e saída ao trabalhar com o request ou response.
+    */
     @Test
     public void testAddEventWithout_adding() throws IOException {
 
@@ -187,6 +210,13 @@ public class StudentCalendarServletTest {
         assertTrue(true);
     }
 
+    /**
+     * Testa a funcionalidade de exclusão de eventos do calendário de estudante.
+     * É criada uma lista de horários que contém um único horário para a unidade curricular Programação,
+     * e em seguida é tentado remover este horário do calendário do estudante usando o método doDelete da
+     * classe StudentCalendarServlet. O teste verifica se a lista de horários do estudante está vazia após a remoção.
+     * @throws IOException se ocorrer um erro de entrada/saída durante a execução do teste
+    */
     @Test
     public void testDeleteEvents() throws IOException {
 
